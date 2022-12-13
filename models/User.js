187003530e300d3d67543868b618eaaa46c8183f
osumsky <osumsky@gmail.com>
2022-12-13 22:07:10 +0200

    Fixed foreign key

--- models/User.js
+++ models/User.js
@@ -11,7 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.hasMany(models.Task);
+      User.hasMany(models.Task,{
+        foreignKey: "userId"
+      });
     }
   }
   User.init(
